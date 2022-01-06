15dccca35d0589efa1fa4f99cd3a15f97cbfb1cf
nguyenhk <nguyenhk@trobz.com>
2022-01-06 16:26:48 +0700

    [LOCAL] config.sh

--- server_env_printing_server/config.sh
+++ server_env_printing_server/config.sh
@@ -19,3 +19,14 @@ GH_NAME=HoKhanhNguyen99
 # Uncomment to enable workers
 # 2 workers do not work well with coverage
 # WORKERS="-d ${DB_PREFIX}_run_${DB_SUFFIX} --workers=0 --load=base,queue_job"
+REPO_MODULES=base_report_to_printer_mail,base_report_to_printer,printer_zpl2,server_env_printing_server
+DEP_TREE=`cat <<EOF
+server_env_printing_server (14.0.1.0.0)
+    ├── base_report_to_printer (14.0.1.1.1)
+    │   └── web (14.0+c)
+    └── server_environment (13.0.3.0.0)
+        └── base_sparse_field (14.0+c)
+EOF
+`
+INIT_MODULES=base_report_to_printer,server_environment
+INIT_REPO_MODULES=mail,server_environment,web
